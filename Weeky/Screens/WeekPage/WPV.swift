a0d19ca68b58ed71662cd9155d7acfd2a80bd364
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-04-11 11:53:39 +0300

    WeekPageView work with

--- Weeky/Screens/WeekPage/WPV.swift
+++ Weeky/Screens/WeekPage/WPV.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct WeekPageView: View {
-    //@StateObject var taskModel = WeekPageViewModel()
-//    @Namespace var animation
+    @StateObject var taskModel = WeekPageViewModel()
+    //    @Namespace var animation
     @State var isShowingSideMenu = false
     @State private var isCalendarViewShowed = false
     
@@ -18,12 +18,119 @@ struct WeekPageView: View {
             NavigationView {
                 VStack {
                     HeaderView(isShowingSideMenu: $isShowingSideMenu, isCalendarViewShowed: $isCalendarViewShowed)
-//                    WeekView()
+                    //                    WeekView()
+//                    TaskView()
                     MainScrollView()
                 }
             }
             SideMenu(isShowingSideMenu: $isShowingSideMenu)
         }
+    }
+    
+    
+    func MainScrollView()->some View {
+        //        var body: some View {
+        //    func MainScrollView()-> some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            WeekView()
+            
+            VStack(spacing: 18){
+                if let tasks = taskModel.filteredTasks{
+                    if tasks.isEmpty{
+                        Text("No Tasks Found!!!")
+                            .font(.system(size: 16))
+                            .fontWeight(.light)
+                        //                        .offset(y: 100)
+                    }else{
+                        ForEach(tasks){ task in
+                            TaskCardView(task: task)
+                        }
+                    }
+                }else{
+                    //Mark: Progress View
+                    ProgressView()
+                        .offset(y: 100)
+                }
+            }
+            //Updating Task
+            .onChange(of: taskModel.currentDay){ newValue in
+                taskModel.filteringTodayTask()
+            }
+            .padding()
+            .padding(.top)
+        }
+        //            TaskCardView(task: Task(id: nil, taskTitle: "kjdkf", taskDescription: "lkjdfkgj", taskDate: nil))
+        //Lazy with pinned header
+    }
+//}
+
+    func TaskCardView(task: Task)->some View{
+        HStack(alignment: .top, spacing: 30){
+            VStack(spacing: 10){
+                Circle()
+                    .fill(.black)
+                    .frame(width: 15, height: 15)
+                    .background(
+                        Circle()
+                            .stroke(.black, lineWidth: 1)
+                            .padding(-3)
+                    )
+                Rectangle()
+                    .fill(.black)
+                    .frame(width: 3)
+                
+            }
+            VStack{
+                HStack(alignment: .top, spacing: 10){
+                    VStack(alignment: .leading, spacing: 12){
+                        Text(task.taskTitle)
+                            .font(.title2.bold())
+                        Text(task.taskDescription)
+                            .font(.callout)
+                            .foregroundStyle(.secondary)
+                    }
+                    .hLeading()
+                    
+                    Text(task.taskDate.formatted(date: .omitted, time: .shortened))
+                }
+                
+                if taskModel.isCurrentHour(date: task.taskDate){
+                    HStack(spacing: 0){
+                        HStack(spacing: -10){
+                            ForEach(["Profile","Profile","Profile"], id: \.self){ user in
+                                Image(user)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 45, height: 45)
+                                    .clipShape(Circle())
+                                    .background(
+                                        Circle()
+                                            .stroke(.black, lineWidth: 5)
+                                    )
+                            }
+                        }
+                        .hLeading()
+                        
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .foregroundStyle(.black)
+                                .padding(10)
+                                .background(Color.white, in: RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                    .padding(.top)
+                }
+            }
+            .foregroundColor(taskModel.isCurrentHour(date: task.taskDate) ? .white : .black)
+            .padding()
+            .hLeading()
+            .background(
+                Color(.black)
+                    .cornerRadius(25)
+            )
+        }.hLeading()
     }
 }
 
@@ -102,16 +209,4 @@ struct WeekView: View {
 
 
 
-struct MainScrollView: View {
-    var body: some View {
-        //    func MainScrollView()-> some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            WeekView()
-            
-            Text("1")
-            Text("1")
-            Text("1")
-            //Lazy with pinned header
-        }
-    }
-}
+
