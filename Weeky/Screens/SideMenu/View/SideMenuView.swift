d36a3e36adb4066f40ea9905e6a21f9d436632c6
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-04-14 17:28:13 +0300

    Zstack x.circle

--- Weeky/Screens/SideMenu/View/SideMenuView.swift
+++ Weeky/Screens/SideMenu/View/SideMenuView.swift
@@ -7,230 +7,98 @@
 
 import SwiftUI
 
-//struct SideMenuView: View {
-//    @Binding var isShowingSideMenu: Bool
-//    
-//    var body: some View {
-//        VStack {
-//            Text("Side Menu")
-//            Button("Close") {
-//                isShowingSideMenu.toggle()
-//            }
-//        }
-//        .padding()
-//        .frame(maxWidth: .infinity, alignment: .leading)
-//        .background(Color.white)
-//        .edgesIgnoringSafeArea(.vertical)
-//    }
-//}
-
-enum tabIcon: String{
-    case Home
-    case Card
-    case Favorite
-    case Purchases
-    case Notification
-}
-
-struct SideBar: Identifiable {
-    var id = UUID()
-    var icon: String
-    var title: String
-    var index: Int
-    var tab: tabIcon
-}
-
-let sidebarItems = [
-    SideBar(icon: "house.fill", title: "Home", index: 0, tab: .Home),
-    SideBar(icon: "creditcard.fill", title: "Card", index: 1, tab: .Card),
-    SideBar(icon: "heart.fill", title: "Favorite", index: 2, tab: .Favorite),
-    SideBar(icon: "cart.fill.badge.plus", title: "Purchases", index: 3, tab: .Purchases),
-    SideBar(icon: "bell.badge.fill", title: "Notification", index: 4, tab: .Notification)
-]
- 
+import SwiftUI
 
 struct SideMenuView: View {
-    
-    @State var selectedItem: tabIcon = .Home
-    @State var yOffset: CGFloat = 0
+//    @Binding var sideMenuParameters : SideMenuParameters
+//    var newPassword = String()
+    @State var editData = false
     
     var body: some View {
-        ZStack{
-            Color("Blue light")
-                .frame(width: 266)
-                .mask(RoundedRectangle(cornerRadius: 10, style: .continuous))
-            VStack(alignment: .leading){
-                VStack(alignment: .leading){
-                    userProfile()
-                    tabView(selectedItem: $selectedItem, yOffset: $yOffset)
-                    
-                    
-                }.padding(.leading, 15)
-                Spacer()
-                divider()
-                WeekSlider()
-                
-                HStack{
-                    Text("Learn more about the app")
-                        .padding(.leading)
-                    Image(systemName: "questionmark.circle")
+        ScrollView {
+            Spacer()
+            VStack {
+                Text("Acount:")
+                    .font(.title)
+                if !editData {
+                    Button("изменить данные", action: {
+                        withAnimation(.smooth) {
+                            editData.toggle()
+                        }
+                    })
+                    .font(.title2)
                 }
-                .foregroundColor(.blue)
-                .padding(.top, 5)
-                Spacer()
-                HStack{
-                    Bicon(icon: "moon.zzz.fill")
-                    Spacer()
-                    Text("Shoping")
-                        .foregroundColor(.green)
-                    Spacer()
-                    Bicon(icon: "gearshape.fill")
+                else {
+                    AccauntSettings()
                 }
-                .padding(.bottom, 20)
-                .frame(width: 230, height: 90)
-                .padding(.leading, 17)
-                
             }
+            Divider()
+//            Spacer()
+//            VStack {
+//            Text("Темная тема / светлая тема")
+//            Spacer()
+//            Text("Уведомления")
+            
+            VStack {
+                Text("Темная тема / светлая тема")
+                Text("Уведомления")
+                Text("день недели")
+            }
+            
+//                Toggle("Switch", isOn: $sideMenuParameters.darkThemeIsOn)
+//                    .padding()
+//                    .toggleStyle(SwitchToggleStyle(tint: .blue)) // Можно настроить стиль переключателя
+//                
+//                Text(sideMenuParameters.darkThemeIsOn ? "Enabled" : "Disabled")
+//                    .padding()
+//            }
+            Divider()
             
         }
-        .ignoresSafeArea()
-        .frame(maxHeight: .infinity)
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
-}
-
-struct sideMenu_Previews: PreviewProvider {
-    static var previews: some View {
-        SideMenuView()
-    }
-}
-
-struct userProfile: View {
-    var body: some View {
-        HStack{
-            Circle()
-                .frame(width: 65, height: 65)
-                .foregroundColor(.purple)
-            VStack{
-                RoundedRectangle(cornerRadius: 3, style: .continuous)
-                    .frame(width: 100, height: 14)
-                RoundedRectangle(cornerRadius: 3, style: .continuous)
-                    .frame(width: 80, height: 7)
-                    .opacity(0.5)
-                RoundedRectangle(cornerRadius: 3, style: .continuous)
-                    .frame(width: 52, height: 7)
-                    .opacity(0.5)
-                
-            }
-        }.foregroundColor(.brown)
-            .padding(.top, 60)
-    }
-}
-
-struct tabView: View {
     
-    @Binding var selectedItem: tabIcon
-    @Binding var yOffset: CGFloat
-    @State var isAnimation = false
-    
-    var body: some View{
-        ZStack(alignment: .leading){
-            Rectangle()
-                .frame(width: isAnimation ? 7 : 230, height: 45)
-                .foregroundColor(Color("Blue dark"))
-                .cornerRadius(7)
-                .offset(y: yOffset)
-                .padding(.vertical, 8)
-                .padding(.horizontal, 5)
-                .offset(y: -125)
-                .offset(x: -20)
-                .animation(.default, value: isAnimation)
-            
-            VStack(spacing: 0){
-                ForEach(sidebarItems) { item in
-                    Button {
-                        withAnimation {
-                            isAnimation = true
+    func AccauntSettings()->some View {
+        VStack {
+            ZStack {
+                VStack {
+                    Spacer()
+                    Text("изменить логин")
+                        .font(.title2)
+                    Text("изменить пароль")
+                        .font(.title2)
+                    Spacer()
+                    Button(action: {
+                        withAnimation(.smooth) {
+                            editData.toggle()
                         }
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
-                            withAnimation {
-                                selectedItem = item.tab
-                                yOffset = CGFloat(item.index) * 70
+                    }, label: {
+                        Image(systemName: "сохранить")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 24))
+                    })
+                    //                    Divider()
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            withAnimation(.smooth) {
+                                editData.toggle()
                             }
-                        }
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6){
-                            withAnimation {
-                                isAnimation = false
-                            }
-                        }
-                        
-                    } label: {
-                        HStack{
-                            ZStack{
-                                Circle()
-                                    .frame(width: 39, height: 40)
-                                    .foregroundStyle(.ultraThinMaterial)
-                                Image(systemName: item.icon)
-                                    .foregroundColor(.green)
-                            }
-                            Text(item.title)
-                                .bold()
-                                .font(.title3)
-                                .foregroundColor(.blue)
-                                .padding(.leading, 10)
-                            Spacer()
-                        }
-                        .padding(.top, 30)
-                        
+                        }, label: {
+                            Image(systemName: "x.circle")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 24))
+                        })
+                        Spacer()
                     }
-                    
                 }
             }
-            .frame(width: 240, height: 330)
-            
-            
+            Button("Выйти из аккаунта", action: {
+                print("ВЫйти")
+            })
+            .foregroundColor(.red)
         }
     }
 }
-
-struct divider: View {
-    var body: some View{
-        Rectangle()
-            .frame(width: 266, height: 1)
-            .foregroundColor(.gray.opacity(0.7))
-            .padding(.top,30)
-    }
-}
-
-struct Bicon: View{
-    
-    var icon = ""
-    
-    var body: some View{
-        ZStack{
-            Circle()
-                .frame(width: 48, height: 48)
-                .foregroundStyle(.ultraThinMaterial)
-            Image(systemName: icon)
-                .foregroundColor(.cyan)
-        }
-    }
-    
-}
-
-
-//struct SideMenuView: View {
-//    var body: some View {
-//        VStack {
-//            Text("Parameter 1")
-//            Text("Parameter 2")
-//            Text("Parameter 3")
-//            Text("Parameter 4")
-//            Text("Parameter 5")
-//        }
-//        .frame(width: 240, height: 330)
-//        .background(Color.blue)
-//    }
-//}
