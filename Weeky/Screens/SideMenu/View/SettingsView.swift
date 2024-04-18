02d3f7eda5138fe2ddbfcae243a193b3d171ed25
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-04-18 10:54:36 +0300

    start with account menu

--- Weeky/Screens/SideMenu/View/SettingsView.swift
+++ Weeky/Screens/SideMenu/View/SettingsView.swift
@@ -13,77 +13,103 @@ struct SettingsView: View {
     @State private var darkModeOn = false
     @State private var notificationsOn = false
     @State private var selectedDayIndex = 0
+    @State private var isShowingAccountView = false
     
     @Binding var isAuthorized : Bool
     
     let daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
     
     var body: some View {
-        ScrollView {
-            HStack {
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                VStack(alignment: .leading) {
-                    Text("\(viewModel.currentUser?.name ?? "_")")
-                        .font(.title)
-                    Text("имя пользователя")
-                        .font(.footnote)
-                        .foregroundColor(.gray)
-                }
-                //                Spacer()
-            }
-            .padding()
-            
-            Divider()
-            
-            VStack {
-                Button(action: {
-                    // Change password
-                }) {
-                    Text("Изменить пароль")
-                }
-                .padding()
-                
-                Button(action: {
-                    // Change username
-                }) {
-                    Text("Изменить имя")
-                }
-            }
-            Divider()
-            //            .padding()
-            
-            VStack {
-                Toggle(isOn: $darkModeOn) {
-                    Text("Темная тема")
-                }
-                .padding()
-                
-                Toggle(isOn: $notificationsOn) {
-                    Text("Уведомления")
-                }
-                .padding()
-                
-                
-                HStack {
-                    Text("День начала недели")
-                        .padding()
+//        if !isShowingAccountView {
+            ScrollView {
+                if isShowingAccountView {
                     Spacer()
-                    Picker(selection: $selectedDayIndex, label: Text("Выберите день")) {
-                        ForEach(0..<daysOfWeek.count) { index in
-                            Text(daysOfWeek[index])
-                        }
-                    }
-                    .padding()
                 }
-                //                .pickerStyle(WheelPickerStyle())
-                .pickerStyle(DefaultPickerStyle())
+                HStack {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    VStack(alignment: .leading) {
+                        Text("\(viewModel.currentUser?.name ?? "_")")
+                            .font(.title)
+                        Text("имя пользователя")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    ForwardArrow()
+                }
+                .padding()
+                
+                if isShowingAccountView {
+                    Divider()
+                    
+                    VStack {
+                        Button(action: {
+                            // Change password
+                        }) {
+                            Text("Изменить имя")
+                        }
+                        .padding()
+                        
+                        Button(action: {
+                            // Change username
+                        }) {
+                            Text("Изменить пароль")
+                        }
+                        .padding()
+                        
+                        Button(action: {
+                            //MARK: - back to authorization View
+                            viewModel.currentUser = nil
+                            viewModel.storedTasks.removeAll()
+                            withAnimation {
+                                isAuthorized = false
+                            }
+                        }) {
+                            Text("Выйти из аккаунта")
+                                .foregroundColor(.red)
+                        }
+                        .padding()
+                    }
+                    Divider()
+                        .padding()
+                } else {
+                    
+                    VStack {
+                        Toggle(isOn: $darkModeOn) {
+                            Text("Темная тема")
+                        }
+                        .padding()
+                        
+                        Toggle(isOn: $notificationsOn) {
+                            Text("Уведомления")
+                        }
+                        .padding()
+                        
+                        
+                        HStack {
+                            Text("День начала недели")
+                                .padding()
+                            Spacer()
+                            Picker(selection: $selectedDayIndex, label: Text("Выберите день")) {
+                                ForEach(0..<daysOfWeek.count) { index in
+                                    Text(daysOfWeek[index])
+                                }
+                            }
+                            .padding()
+                        }
+                        //                .pickerStyle(WheelPickerStyle())
+                        .pickerStyle(DefaultPickerStyle())
+                    }
+                    //                .padding()
+                }
+                
+                if !isShowingAccountView {
+                    Spacer()
+                }
             }
-            //                .padding()
-        }
-            Spacer()
-            
+        if !isShowingAccountView {
             Button(action: {
                 //MARK: - back to authorization View
                 viewModel.currentUser = nil
@@ -94,7 +120,76 @@ struct SettingsView: View {
             }) {
                 Text("Выйти из аккаунта")
                     .foregroundColor(.red)
+                //                if isShowingAccountView {
+                //                    Spacer()
+                //                }
             }
+        }
+//        }
+//        else {
+//            AccountView()
+//        }
+    }
+    
+//    func AccountView()->some View {
+//        VStack {
+//            HStack {
+//                Image(systemName: "person.circle")
+//                    .resizable()
+//                    .frame(width: 50, height: 50)
+//                VStack(alignment: .leading) {
+//                    Text("\(viewModel.currentUser?.name ?? "_")")
+//                        .font(.title)
+//                    Text("имя пользователя")
+//                        .font(.footnote)
+//                        .foregroundColor(.gray)
+//                }
+//                ForwardArrow()
+//            }
+//            .padding()
+//            
+//            Divider()
+//            
+//            VStack {
+//                Button(action: {
+//                    // Change password
+//                }) {
+//                    Text("Изменить имя")
+//                }
+//                .padding()
+//                
+//                Button(action: {
+//                    // Change username
+//                }) {
+//                    Text("Изменить пароль")
+//                }
+//            }
+//            Divider()
+//                .padding()
+//        }
+//    }
+    
+    func ForwardArrow()->some View {
+//        Spacer()
+        HStack {
+            Button(action: {
+                withAnimation(.smooth) {
+                    withAnimation {
+                        isShowingAccountView.toggle()
+                    }
+                }
+            }, label: {
+                Image(systemName: !isShowingAccountView ? "chevron.forward" : "x.circle" )
+//                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.black)
+//                Spacer()
+            })
+        }
+        .padding(30)
     }
 }
+
+
 
