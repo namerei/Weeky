f2dc0561b1f8f98f1c5a300a38f5d124048f1151
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-04-21 20:02:39 +0300

    work on settings menu

--- Weeky/Screens/SideMenu/View/SettingsView.swift
+++ Weeky/Screens/SideMenu/View/SettingsView.swift
@@ -33,7 +33,7 @@ struct SettingsView: View {
     @State private var language = false
     @State private var selectedDayIndex = 0
     @State private var isShowingAccountView = false
-    @State var isShowingSuccessView = false
+    @State private var isShowingSuccessView = false
     @State private var error = "Пустые поля"
     
     @State private var name = ""
@@ -44,95 +44,93 @@ struct SettingsView: View {
     let daysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
     
     var body: some View {
-//        if !isShowingAccountView {
         NavigationView {
-            List {
-                if isShowingAccountView {
-                    Spacer()
-                }
-                HStack {
-//                    Image(systemName: "person.circle")
-//                        .resizable()
-//                        .frame(width: 50, height: 50)
-                    VStack(alignment: .leading) {
-                        Text("\(viewModel.currentUser?.name ?? "_")")
-                            .font(.title)
-                        Text("Аккаунт")
-                            .font(.footnote)
-                            .foregroundColor(.gray)
-                    }
-                    Spacer()
-                    ForwardArrow()
-                }
-                .padding()
-                
-                if isShowingAccountView {
-                    ChangeUserData()
-                } else {
-                    
-                    VStack {
-                        //                        withAnimation {
-                        Toggle(isOn: withAnimation {$viewModel.isDarkMode}) {
-                            Text("Темная тема")
-                        }
-                        .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
-                        .padding()
-                        
-                        Toggle(isOn: $notificationsOn) {
-                            Text("Уведомления")
-                        }
-                        .padding()
-                        
-                        Toggle(isOn: $language) {
-                            Text("Изменить язык (ru/en)")
-                        }
-                        .padding()
-                        
-                        
-                        HStack {
-                            Text("День начала недели")
-                                .padding()
-                            Spacer()
-                            Picker(selection: $selectedDayIndex, label: Text("")) {
-                                ForEach(0..<daysOfWeek.count) { index in
-                                    Text(daysOfWeek[index])
-                                }
-                            }
-                            .padding()
-                        }
-                        //                .pickerStyle(WheelPickerStyle())
-                        .pickerStyle(DefaultPickerStyle())
-                    }
-                    //                .padding()
-                }
-                
-                if !isShowingAccountView {
-                    Spacer()
-                }
-            }
-            if !isShowingAccountView {
-                Button(action: {
-                    //MARK: - back to authorization View
-                    viewModel.currentUser = nil
-                    viewModel.storedTasks.removeAll()
-                    withAnimation {
-                        isAuthorized = false
-                    }
-                }) {
-                    Text("Выйти из аккаунта")
-                        .foregroundColor(.red)
-                    //                if isShowingAccountView {
-                    //                    Spacer()
-                    //                }
-                }
+            VStack {
+                MainListView()
+                LogoutButton()
             }
         }
         .background(Color("Blue xlight").edgesIgnoringSafeArea(.all))
         .foregroundColor(Color(.black))
         .navigationBarTitle("Настройки", displayMode: .large)
-
+        
     }
     
+    //MARK: - Views
+    func MainListView()->some View {
+        List {
+            VStack {
+                HStack {
+                    //                    Image(systemName: "person.circle")
+                    //                        .resizable()
+                    //                        .frame(width: 50, height: 50)
+                    //                                Text("\(viewModel.currentUser?.name ?? "_")")
+                    //                                    .font(.title)
+                    Text("Аккаунт")
+                    Spacer()
+                    Image(systemName: "chevron.forward")
+//                    ForwardArrow()
+                }
+                .onTapGesture {
+                    print("TAp")
+                }
+                Divider()
+                //                    .padding()
+                
+                Toggle(isOn: withAnimation {$viewModel.isDarkMode}) {
+                    Text("Темная тема")
+                }
+//                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
+                Divider()
+                //                    .padding()
+                
+                Toggle(isOn: $notificationsOn) {
+                    Text("Уведомления")
+                }
+//                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
+                Divider()
+                //                    .padding()
+                
+                Toggle(isOn: $language) {
+                    Text("Изменить язык (ru/en)")
+                }
+//                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
+                Divider()
+                //                    .padding()
+                
+                //                    HStack {
+                //                        Text("День начала недели")
+                //                            .padding()
+//                Spacer()
+                Picker(selection: $selectedDayIndex, label: Text("День начала недели")) {
+                    ForEach(0..<daysOfWeek.count) { index in
+                        Text(daysOfWeek[index])
+                    }
+                }
+//                .foregroundColor(Color("Blue xlight"))
+                .pickerStyle(DefaultPickerStyle())
+            }
+            //                .padding()
+        }
+//        .disabled(true)
+//        .listStyle(InsetListStyle())
+    }
+    
+    func LogoutButton()->some View {
+        Button(action: {
+            viewModel.currentUser = nil
+            viewModel.storedTasks.removeAll()
+            withAnimation {
+                isAuthorized = false
+            }
+        }) {
+            Text("Выйти из аккаунта")
+                .foregroundColor(.red)
+            //                if isShowingAccountView {
+            //                    Spacer()
+            //                }
+        }
+    }
     
     func ForwardArrow()->some View {
 //        Spacer()
@@ -144,10 +142,11 @@ struct SettingsView: View {
                     }
                 }
             }, label: {
-                Image(systemName: !isShowingAccountView ? "chevron.forward" : "x.circle" )
+//                Image(systemName: !isShowingAccountView ? "chevron.forward" : "x.circle" )
+                Image(systemName: "chevron.forward")
 //                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 20, height: 20)
+//                    .aspectRatio(contentMode: .fill)
+//                    .frame(width: 20, height: 20)
                     .foregroundColor(.black)
 //                Spacer()
             })
@@ -239,3 +238,16 @@ struct SettingsView: View {
     }
 }
                         
+struct Previews: PreviewProvider {
+    
+//    struct Container: View {
+    static var previews: some View {
+        @State var show = true
+        //        @State var cityName = "Мончегорск"
+        
+//        var body: some View {
+            //            DestinationSearchView(show: $show, cityName: $cityName)
+            SettingsView(isAuthorized: $show)
+        }
+//    }
+}
