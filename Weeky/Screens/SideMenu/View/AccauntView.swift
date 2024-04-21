2723c985fb379922306629e889bbd78d6d1fb3eb
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-04-22 01:05:41 +0300

    AccountView

--- Weeky/Screens/SideMenu/View/AccauntView.swift
+++ Weeky/Screens/SideMenu/View/AccauntView.swift
@@ -7,3 +7,114 @@
 
 import SwiftUI
 
+struct AccountView:View {
+    @EnvironmentObject var viewModel : HomeViewModel
+    @State private var isShowingSuccessView = false
+    
+    @State private var name = ""
+    @State private var password = ""
+    
+    @Binding var isAuthorized : Bool
+    @State private var error = "Пустые поля"
+    
+    var body: some View {
+        VStack {
+            ChangeUserData()
+        }
+    }
+    
+    func ChangeUserData()->some View {
+        VStack {
+            VStack{
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                Text(viewModel.currentUser?.name ?? "-")
+                    .font(.largeTitle)
+            }
+            .frame(width: 400, height: 160)
+            .background(Color("Blue dark"))
+//            .padding(.horizontal)
+//            .padding(.top, 40)
+            
+            VStack {
+                HStack(spacing: 15) {
+                    Image(systemName: "eye.slash.fill")
+                        .foregroundColor(Color("Blue light"))
+                    SecureField("Изменить пароль", text: $password)
+                }
+                
+                Divider()
+                    .background(Color.white.opacity(0.6))
+            } .padding(.horizontal)
+                .padding(.top, 50)
+            
+            AcceptButton()
+            
+            Button(action: {
+                //MARK: - back to authorization View
+                
+//                viewModel.currentUser = nil
+//                viewModel.storedTasks.removeAll()
+                
+//                viewModel.
+                withAnimation {
+                    isAuthorized = false
+                }
+            }) {
+                Text("Выйти из аккаунта")
+                    .foregroundColor(.red)
+            }
+            .padding()
+            Spacer()
+        }
+    }
+    
+    func AcceptButton()->some View {
+        VStack {
+            if !isShowingSuccessView {
+                Button(action: {
+                    if name != "" || password != "" {
+                        //MARK: - update Data
+                        viewModel.updatePassword( password)
+                        error = ""
+                        withAnimation {
+                            isShowingSuccessView = true
+                            name = ""
+                            password = ""
+                        }
+                    }
+                    isShowingSuccessView = true
+                }, label: {
+                    Text("Внести изменения")
+                        .foregroundColor(.gray)
+                        .fontWeight(.medium)
+                        .padding()
+                        .clipShape(Capsule())
+                        .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+                                        .offset(y: 25)
+                })
+            } else {
+                SuccessView(text: "Данные изменены", error: error)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            withAnimation {
+                                isShowingSuccessView = false
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct Previews_AccountView: PreviewProvider {
+    
+//    struct Container: View {
+    static var previews: some View {
+        @State var show = true
+        
+        AccountView(isAuthorized: $show)
+        }
+//    }
+}
--- Weeky/Screens/SideMenu/View/SettingsView.swift
+++ Weeky/Screens/SideMenu/View/SettingsView.swift
@@ -7,23 +7,6 @@
 
 import SwiftUI
 
-//struct CustomToggleStyle: ToggleStyle {
-//    var color: Color
-//    
-//    func makeBody(configuration: Configuration) -> some View {
-//        Button {
-//            configuration.isOn.toggle()
-//        } label: {
-//            HStack {
-//                configuration.label
-//                    .foregroundColor(color)
-//                Spacer()
-//                Image(systemName: configuration.isOn ? "checkmark.square.fill" : "square")
-//                    .foregroundColor(color)
-//            }
-//        }
-//    }
-//}
 
 struct SettingsView: View {
     @EnvironmentObject var viewModel : HomeViewModel
@@ -33,25 +16,23 @@ struct SettingsView: View {
     @State private var language = false
     @State private var selectedDayIndex = 0
     @State private var isShowingAccountView = false
-    @State private var isShowingSuccessView = false
-    @State private var error = "Пустые поля"
-    
-    @State private var name = ""
-    @State private var password = ""
+
     
     @Binding var isAuthorized : Bool
     
     let daysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
     
     var body: some View {
-        NavigationView {
-            VStack {
+//        NavigationView {
+        VStack {
                 MainListView()
                 LogoutButton()
             }
-        }
+//        }
         .foregroundColor(Color(.black))
         .navigationBarTitle("Настройки", displayMode: .large)
+//        .navigationBarBackButtonHidden(isShowingAccountView)
+//        .navigatiobar
         
     }
     
@@ -59,53 +40,33 @@ struct SettingsView: View {
     func MainListView()->some View {
         List {
 //            VStack {
-                HStack {
-                    //                    Image(systemName: "person.circle")
-                    //                        .resizable()
-                    //                        .frame(width: 50, height: 50)
-                    //                                Text("\(viewModel.currentUser?.name ?? "_")")
-                    //                                    .font(.title)
-                    Text("Аккаунт")
-                    Spacer()
-                    Image(systemName: "chevron.forward")
-//                    ForwardArrow()
-                }
-                .onTapGesture {
-                    print("TAP")
-                }
+            
+            
+            NavigationLink(destination: AccountView(isAuthorized: $isAuthorized)) {
+                Text("Аккаунт")
+            }
+            //            .onTapGesture {
+//                isShowingAccountView = true
+//            }
 //                .onTapGesture {
-//                    self.selectedItem = item
-//                    // Здесь можно выполнить нужное действие при выборе элемента
-//                    print("Selected item: \(item)")
+//                    print("TAP")
 //                }
-//                Divider()
-                //                    .padding()
-                
+            
                 Toggle(isOn: withAnimation {$viewModel.isDarkMode}) {
                     Text("Темная тема")
                 }
 //                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
-//                Divider()
-                //                    .padding()
-                
+
                 Toggle(isOn: $notificationsOn) {
                     Text("Уведомления")
                 }
 //                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
-//                Divider()
-                //                    .padding()
-                
+
                 Toggle(isOn: $language) {
                     Text("Изменить язык (ru/en)")
                 }
 //                .toggleStyle(SwitchToggleStyle(tint: Color("Blue dark")))
-//                Divider()
-                //                    .padding()
-                
-                //                    HStack {
-                //                        Text("День начала недели")
-                //                            .padding()
-//                Spacer()
+
                 Picker(selection: $selectedDayIndex, label: Text("День начала недели")) {
                     ForEach(0..<daysOfWeek.count) { index in
                         Text(daysOfWeek[index])
@@ -114,10 +75,6 @@ struct SettingsView: View {
 //                .foregroundColor(Color("Blue xlight"))
                 .pickerStyle(DefaultPickerStyle())
             }
-            //                .padding()
-//        }
-//        .disabled(true)
-//        .listStyle(InsetListStyle())
     }
     
     func LogoutButton()->some View {
@@ -130,114 +87,6 @@ struct SettingsView: View {
         }) {
             Text("Выйти из аккаунта")
                 .foregroundColor(.red)
-            //                if isShowingAccountView {
-            //                    Spacer()
-            //                }
-        }
-    }
-    
-    func ForwardArrow()->some View {
-//        Spacer()
-        HStack {
-            Button(action: {
-                withAnimation(.smooth) {
-                    withAnimation {
-                        isShowingAccountView.toggle()
-                    }
-                }
-            }, label: {
-//                Image(systemName: !isShowingAccountView ? "chevron.forward" : "x.circle" )
-                Image(systemName: "chevron.forward")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 20, height: 20)
-                    .foregroundColor(.black)
-//                Spacer()
-            })
-        }
-        .padding(30)
-    }
-    
-    func ChangeUserData()->some View {
-        VStack {
-//            VStack{
-//                HStack(spacing: 15){
-//                    Image(systemName: "person.fill")
-//                        .foregroundColor(Color("Blue light"))
-//                    TextField("Изменить имя пользователя", text: $name)
-//                }
-//                
-//                Divider().background(Color.white.opacity(0.6))
-//            }
-//            .padding(.horizontal)
-//            .padding(.top, 40)
-            
-            VStack {
-                HStack(spacing: 15) {
-                    Image(systemName: "eye.slash.fill")
-                        .foregroundColor(Color("Blue light"))
-                    SecureField("Изменить пароль", text: $password)
-                }
-                
-                Divider()
-                    .background(Color.white.opacity(0.6))
-            } .padding(.horizontal)
-                .padding(.top, 50)
-            
-            AcceptButton()
-            
-            Button(action: {
-                //MARK: - back to authorization View
-                
-//                viewModel.currentUser = nil
-//                viewModel.storedTasks.removeAll()
-                
-//                viewModel.
-                withAnimation {
-                    isAuthorized = false
-                }
-            }) {
-                Text("Выйти из аккаунта")
-                    .foregroundColor(.red)
-            }
-            .padding()
-        }
-    }
-    
-    func AcceptButton()->some View {
-        VStack {
-            if !isShowingSuccessView {
-                Button(action: {
-                    if name != "" || password != "" {
-                        //MARK: - update Data
-                        viewModel.updatePassword( password)
-                        error = ""
-                        withAnimation {
-                            isShowingSuccessView = true
-                            name = ""
-                            password = ""
-                        }
-                    }
-                    isShowingSuccessView = true
-                }, label: {
-                    Text("Внести изменения")
-                        .foregroundColor(.gray)
-                        .fontWeight(.medium)
-                        .padding()
-                        .clipShape(Capsule())
-                        .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
-                                        .offset(y: 25)
-                })
-            } else {
-                SuccessView(text: "Данные изменены", error: error)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            withAnimation {
-                                isShowingSuccessView = false
-                            }
-                        }
-                    }
-            }
         }
     }
 }
@@ -247,10 +96,7 @@ struct Previews: PreviewProvider {
 //    struct Container: View {
     static var previews: some View {
         @State var show = true
-        //        @State var cityName = "Мончегорск"
         
-//        var body: some View {
-            //            DestinationSearchView(show: $show, cityName: $cityName)
             SettingsView(isAuthorized: $show)
         }
 //    }
